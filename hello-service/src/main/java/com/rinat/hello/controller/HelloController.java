068928041a1743317477f16521ebeddc12f8ba4d
Rinat Temirgaliev <rtemirgaliev@gmail.com>
2017-03-24 17:41:21 +0300

    first commit

---
+++ hello-service/src/main/java/com/rinat/hello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.rinat.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    String getHello() {
+        return "Hello";
+    }
+}
