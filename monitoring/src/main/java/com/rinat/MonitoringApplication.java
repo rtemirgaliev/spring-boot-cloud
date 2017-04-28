791cd85b79f4e475c199ec482a67a428b93c32bc
Rinat Temirgaliev <rtemirgaliev@gmail.com>
2017-04-28 18:47:27 +0300

    monitoring, wrong ports

--- monitoring/src/main/java/com/rinat/MonitoringApplication.java
+++ monitoring/src/main/java/com/rinat/MonitoringApplication.java
@@ -3,8 +3,10 @@ package com.rinat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
+@EnableTurbineStream
 @EnableHystrixDashboard
 public class MonitoringApplication {
 
